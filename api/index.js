bc35ad5cd29d3f4dd59d82a8fd741bad8a5ba2fc
Sandeep-K-A <kasandeep42@gmail.com>
2024-05-08 23:15:21 +0530

    created test api routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dbConnection from "./utils/databaseConnection.js";
+import userRouter from "./routes/user.route.js";
 
 const PORT = 3000;
-
 const app = express();
+app.use('/', userRouter)
 
 dbConnection();
 app.listen(PORT, () => {
